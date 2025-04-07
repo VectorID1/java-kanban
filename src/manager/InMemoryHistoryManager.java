bf8a1c96c6a4d794be207b2f472245e8c923dcd4
Alexey.R <VectorID1@yandex.ru>
2025-04-07 14:04:06 +0300

    В класе InMemoryHistoryManager Поправил код стайл.  Убрал лишние условия. Убрал условия в удалении узла на проверку есть ли такой узел. - строка 50 - 52. Присвои first значение null - 66 строка. Закоментил 66 строку, с этим значением метод не работает.

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -5,9 +5,9 @@ import model.Task;
 import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private HashMap<Integer, Node<Task>> historyTask = new HashMap<>();
-    private Node<Task> first = null;
-    private Node<Task> last = null;
+    private  HashMap<Integer, Node<Task>> historyTask = new HashMap<>();
+    private  Node<Task> first = null;
+    private  Node<Task> last = null;
 
     public void linkLast(Task task) {
         Node<Task> newNode = new Node<>(last, task, null);
@@ -30,6 +30,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     public void add(Task task) {
         if (historyTask.containsKey(task.getIdTask())) {
             removeNode(historyTask.get(task.getIdTask()));
+            linkLast(task);
         }
         linkLast(task);
     }
@@ -47,23 +48,23 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     public void removeNode(Node<Task> node) {
         Task task = node.getTask();
-        Node<Task> removeNode = historyTask.get(task.getIdTask());
-        if (removeNode.getNext() != null && removeNode.getPrev() != null) {
-            Node<Task> nextNode = removeNode.getNext();
-            Node<Task> prevNode = removeNode.getPrev();
-            nextNode.setPrev(prevNode);
-            prevNode.setNext(nextNode);
-        } else if (removeNode.getNext() == null && removeNode.getPrev() != null) {
-            Node<Task> prevNode = removeNode.getPrev();
-            prevNode.setNext(null);
-            last = prevNode;
-        } else if (removeNode.getPrev() == null && removeNode.getNext() != null) {
-            Node<Task> firstNode = removeNode.getNext();
-            firstNode.setPrev(null);
-            first = firstNode;
-        } else
-            last = null;
-        first = null;
+            Node<Task> removeNode = historyTask.get(task.getIdTask());
+            if (removeNode.getNext() != null && removeNode.getPrev() != null) {
+                Node<Task> nextNode = removeNode.getNext();
+                Node<Task> prevNode = removeNode.getPrev();
+                nextNode.setPrev(prevNode);
+                prevNode.setNext(nextNode);
+            } else if (removeNode.getNext() == null && removeNode.getPrev() != null) {
+                Node<Task> prevNode = removeNode.getPrev();
+                prevNode.setNext(null);
+                last = prevNode;
+            } else if (removeNode.getPrev() == null && removeNode.getNext() != null) {
+                Node<Task> firstNode = removeNode.getNext();
+                firstNode.setPrev(null);
+                first = firstNode;
+            } else
+                last = null;
+               // first = null;  Если раскоментить метод не удаляет...
         historyTask.remove(task.getIdTask());
     }
 
